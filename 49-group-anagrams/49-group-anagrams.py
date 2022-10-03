d7197d844361c0eddd15bb59b081164196201291
Michael Joson <98543433+MichaelJoson@users.noreply.github.com>
2022-10-03 16:50:11 -0500

    Time: 153 ms (67.98%), Space: 17.8 MB (67.43%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -11,32 +11,3 @@ class Solution:
             else:
                 anagrams[sortedWord] = [x]
         return anagrams.values()
-        '''
-        for x in range(len(strs)):
-            anagrams = []
-            if strs[x] not in added:
-                for y in range(x+1, len(strs)):
-                    currentStr = strs[x]
-                    nextStr = strs[y]
-                    a = []
-                    b = []
-                    for j in currentStr:
-                        a.append(j)
-                    for k in nextStr:
-                        b.append(k)
-                    a.sort()
-                    b.sort()
-                    if a==b:
-                        if currentStr not in anagrams:
-                            anagrams.append(currentStr)
-                        anagrams.append(nextStr)
-                        added.append(currentStr)
-                        added.append(nextStr)
-            if len(anagrams) > 1:
-                result.append(anagrams)
-            else:
-                result.append(strs[x])
-        return result
-        
-        '''
-        
